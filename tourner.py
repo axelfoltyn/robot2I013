02085e2aac70813c5344fa875a57ff2b46b10d8f
VicVance188 <46991759+VicVance188@users.noreply.github.com>
2019-01-30 17:32:31 +0100

    sauvegarde de l'ancienne direction

--- tourner.py
+++ tourner.py
@@ -8,4 +8,6 @@ def tourner(teta=90):
 	dy = self.direction[1]
 	self.direction[0] = dx*math.cos(trad) - dy*math.sin(trad)
 	self.direction[1] = dx*math.sin(trad) + dy*math.sin(trad)
+	self.old_direction[0] = dx
+	self.old_direction[1] = dy
 	return
