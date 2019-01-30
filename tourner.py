4ec7e8f5906553726ddc664aaf812b3231b740a9
vicvance188 <46991759+vicvance188@users.noreply.github.com>
2019-01-30 18:23:45 +0100

    attributs mis en private

--- tourner.py
+++ tourner.py
@@ -2,18 +2,18 @@ import math
 
 #a mettre dans la classe robot
 
-def tourner(teta=90):
+def tourner(self, teta=90):
 	trad = math.radian(teta)
-	dx = self.direction[0]
-	dy = self.direction[1]
-	self.direction[0] = dx*math.cos(trad) - dy*math.sin(trad)
-	self.direction[1] = dx*math.sin(trad) + dy*math.sin(trad)
-	vx = self.vitesse[0]
-	vy = self.vitesse[1]
-	self.vitesse[0] = vx*math.cos(trad) - vy*math.sin(trad)
-	self.vitesse[1] = vx*math.sin(trad) + vy*math.sin(trad)
-	ax = self.acceleration[0]
-	ay = self.acceleration[1]
-	self.acceleraion[0] = ax*math.cos(trad) - ay*math.sin(trad)
-	self.acceleraion[1] = ax*math.sin(trad) + ay*math.sin(trad)
+	dx = self._direction[0]
+	dy = self._direction[1]
+	self._direction[0] = dx*math.cos(trad) - dy*math.sin(trad)
+	self._direction[1] = dx*math.sin(trad) + dy*math.sin(trad)
+	vx = self._vitesse[0]
+	vy = self._vitesse[1]
+	self._vitesse[0] = vx*math.cos(trad) - vy*math.sin(trad)
+	self._vitesse[1] = vx*math.sin(trad) + vy*math.sin(trad)
+	ax = self._acceleration[0]
+	ay = self._acceleration[1]
+	self._acceleraion[0] = ax*math.cos(trad) - ay*math.sin(trad)
+	self._acceleraion[1] = ax*math.sin(trad) + ay*math.sin(trad)
 	return
