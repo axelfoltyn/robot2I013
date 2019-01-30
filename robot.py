823ad24857232756f487234b9039b96edf054a4c
axelfoltyn <axel.foltyn@epita.fr>
2019-01-30 18:32:35 +0100

    mis un tableau de position +ajout valeur de base a acceleration

--- robot.py
+++ robot.py
@@ -4,12 +4,12 @@ import numpy as np
 
 class Robot:
 
-    def __init__(self,acceleration, x=0, y=0, z=0):  # x et y sont labscisse et lordonné
+    def __init__(self,acceleration=0, x=0, y=0, z=0):  # x et y sont labscisse et lordonné
                 """self*int*int*int->none"""
           fichier=open("robot.txt","r")
           f=fichier.readlines()
           f=f.strip() #suppression du retour a la ligne
-          self._x, self_y=[int(elt) for elt in f.split(";")]
+          self._position =[int(elt) for elt in f.split(";")]
           #self._x=x/2
           #self._y=y/2                                           # initialise le robot au mileu de la map
           self._acceleration=[0.0,0.0,0.0]   #on initialise acceleration a la vitesse max que l'on lui attribue
