cd4ac455af44e2c6ec70dc042ceaab1fe365c4c5
axelfoltyn <axel.foltyn@epita.fr>
2019-01-31 08:19:00 +0100

    indentation

--- robot.py
+++ robot.py
@@ -44,10 +44,10 @@ class Robot:
         
 
 
-#a mettre dans la classe robot
+    #a mettre dans la classe robot
 
-def tourner(self, teta=90):
-	trad = math.radian(teta)
+    def tourner(self, teta=90):
+        trad = math.radian(teta)
 	dx = self._direction[0]
 	dy = self._direction[1]
 	self._direction[0] = dx*math.cos(trad) - dy*math.sin(trad)
