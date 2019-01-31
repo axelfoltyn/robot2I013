f142be9d3ebcbe2eb6230ed2786f120487bb5e0b
SophiaNdr <46903185+SophiaNdr@users.noreply.github.com>
2019-01-31 11:02:38 +0100

    Update Acceleration.py

--- Acceleration.py
+++ Acceleration.py
@@ -1,6 +1,6 @@
 def acceleratioon(acceleration):
-    self._acceleration[0]+=acceleration[0]
-    self._acceleration[1]+=acceleration[1]
-    self._acceleration[2]+=acceleration[2]
+    self._acceleration[0]+=acceleration[0]*direction[0]
+    self._acceleration[1]+=acceleration[1]*direction[1]
+    self._acceleration[2]+=acceleration[2]*direction[2]
 
    
