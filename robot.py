c29fbe9bc39aed4e5deeb637142e035f9f7a9d8a
vicvance188 <46991759+vicvance188@users.noreply.github.com>
2019-01-31 10:50:02 +0100

    maj fonction tourner

--- robot.py
+++ robot.py
@@ -49,15 +49,7 @@ class Robot:
         dy = self._direction[1]
         self._direction[0] = dx*math.cos(trad) - dy*math.sin(trad)
         self._direction[1] = dx*math.sin(trad) + dy*math.sin(trad)
-        vx = self._vitesse[0]
-        vy = self._vitesse[1]
-        self._vitesse[0] = vx*math.cos(trad) - vy*math.sin(trad)
-        self._vitesse[1] = vx*math.sin(trad) + vy*math.sin(trad)
-        ax = self._acceleration[0]
-        ay = self._acceleration[1]
-        self._acceleration[0] = ax*math.cos(trad) - ay*math.sin(trad)
-        self._acceleration[1] = ax*math.sin(trad) + ay*math.sin(trad)
-
+        
     def acceleration(self, acceleration):
         self._acceleration[0]+=acceleration
         self._acceleration[1]+=acceleration
