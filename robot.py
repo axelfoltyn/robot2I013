9c58b6bf9dc3425800228866e0e5f4ee2ec63200
axelfoltyn <axel.foltyn@epita.fr>
2019-01-31 08:56:05 +0100

    ajout duscript fini et du robot

--- robot.py
+++ robot.py
@@ -1,23 +1,20 @@
 #tous les attribus sont protected
-import str
 import math
 import numpy as np
 
 class Robot:
 
-    def __init__(self,acceleration=0, x=0, y=0, z=0):  # x et y sont labscisse et lordonné
-                """self*int*int*int->none"""
-          fichier=open("robot.txt","r")
-          f=fichier.readlines()
-          f=f.strip() #suppression du retour a la ligne
-          self._position =[int(elt) for elt in f.split(";")]
-          #self._x=x/2
-          #self._y=y/2                                           # initialise le robot au mileu de la map
-          self._acceleration=[0.0,0.0,0.0]   #on initialise acceleration a la vitesse max que l'on lui attribue
-          self._vitesse=[0.0,0.0,0.0]
-          self._direction=[0.0,0.0,0.0]
-          self._old_direction=[0.0,0.0,0.0]
-          fichier.close()
+    def __init__(self):
+        fichier=open("robot.txt","r")
+        f=fichier.readlines()
+        #f=f.strip() #suppression du retour a la ligne
+        #self._position =[int(elt) for elt in f.split(";",";")]
+        self._position = [0,0,0]
+        self._acceleration=[0.0,0.0,0.0]   #on initialise acceleration a la vitesse max que l'on lui attribue
+        self._vitesse=[0.0,0.0,0.0]
+        self._direction=[1.0,0.0,0.0]
+        self._old_direction=[0.0,0.0,0.0]
+        fichier.close()
 
 
     #a mettre dans la classe robot
@@ -41,23 +38,28 @@ class Robot:
         self._position[0] = self._position[0] * self._direction[0] + dt * self._vitesse[0]
         self._position[1] = self._position[1] * self._direction[1] + dt * self._vitesse[1]
         self._position[2] = self._position[2] * self._direction[2] + dt * self._vitesse[2]
-        
+    
 
 
     #a mettre dans la classe robot
 
     def tourner(self, teta=90):
-        trad = math.radian(teta)
-	dx = self._direction[0]
-	dy = self._direction[1]
-	self._direction[0] = dx*math.cos(trad) - dy*math.sin(trad)
-	self._direction[1] = dx*math.sin(trad) + dy*math.sin(trad)
-	vx = self._vitesse[0]
-	vy = self._vitesse[1]
-	self._vitesse[0] = vx*math.cos(trad) - vy*math.sin(trad)
-	self._vitesse[1] = vx*math.sin(trad) + vy*math.sin(trad)
-	ax = self._acceleration[0]
-	ay = self._acceleration[1]
-	self._acceleraion[0] = ax*math.cos(trad) - ay*math.sin(trad)
-	self._acceleraion[1] = ax*math.sin(trad) + ay*math.sin(trad)
-	return
+        trad = math.radians(teta)
+        dx = self._direction[0]
+        dy = self._direction[1]
+        self._direction[0] = dx*math.cos(trad) - dy*math.sin(trad)
+        self._direction[1] = dx*math.sin(trad) + dy*math.sin(trad)
+        vx = self._vitesse[0]
+        vy = self._vitesse[1]
+        self._vitesse[0] = vx*math.cos(trad) - vy*math.sin(trad)
+        self._vitesse[1] = vx*math.sin(trad) + vy*math.sin(trad)
+        ax = self._acceleration[0]
+        ay = self._acceleration[1]
+        self._acceleration[0] = ax*math.cos(trad) - ay*math.sin(trad)
+        self._acceleration[1] = ax*math.sin(trad) + ay*math.sin(trad)
+
+    def acceleration(self, acceleration):
+        self._acceleration[0]+=acceleration[0]
+        self._acceleration[1]+=acceleration[1]
+        self._acceleration[2]+=acceleration[2]
+
