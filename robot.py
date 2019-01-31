861bb4a3032dbd14c0a09ad05a2bc98981f6ad4f
SophiaNdr <46903185+SophiaNdr@users.noreply.github.com>
2019-01-31 11:12:26 +0100

    Update robot.py

--- robot.py
+++ robot.py
@@ -43,7 +43,7 @@ class Robot:
         self._direction[1] = dx*math.sin(trad) + dy*math.sin(trad)
         
     def acceleratioon(acceleration):
-        self._acceleration[0]+=acceleration[0]*direction[0]
-        self._acceleration[1]+=acceleration[1]*direction[1]
-        self._acceleration[2]+=acceleration[2]*direction[2]
+        self._acceleration[0]+=acceleration*direction[0]
+        self._acceleration[1]+=acceleration*direction[1]
+        self._acceleration[2]+=acceleration*direction[2]
 
