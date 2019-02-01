636791dbbce5b6c8a268033911b6b87e94daad19
vicvance188 <46991759+vicvance188@users.noreply.github.com>
2019-02-01 17:21:58 +0100

    déjà dans classe robot

--- Acceleration.py
+++
@@ -1,6 +0,0 @@
-def acceleratioon(acceleration):
-    self._acceleration[0]+=acceleration[0]*direction[0]
-    self._acceleration[1]+=acceleration[1]*direction[1]
-    self._acceleration[2]+=acceleration[2]*direction[2]
-
-   
