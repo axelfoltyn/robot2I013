0528670cfad6713c2f0bf43a0631d2bb49264ff0
vicvance188 <46991759+vicvance188@users.noreply.github.com>
2019-02-01 17:34:51 +0100

    Update View.py

--- View.py
+++ View.py
@@ -1,12 +1,11 @@
-from Arene import *
 from tkinter import *
 
 class View:
 	def __init__(self,x=800, y=600):
 		"""intxint -> View"""
-		fenetre = Tk()
 		self._x = x
 		self._y = y
+		fenetre = Tk()
 		canvas = Canvas(fenetre, width=x, height=y, background='white')
 		canvas.pack()
 
