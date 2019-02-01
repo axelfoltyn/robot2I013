52698cfd5874496aa2ee888daf3e9a0b2e390bb3
dyroxe <42959400+dyroxe@users.noreply.github.com>
2019-02-01 15:39:31 +0100

    creation de abstacle

---
+++ obstacle.py
@@ -0,0 +1,9 @@
+class Obstacle :
+	
+	def __init__(self,x,y,z,r,lo,la):
+		self._x=x
+		self._y=y
+		self._z=z
+		self._r=r				#si r est different de 0 alors cest un cercle sinon autre
+		self._lo=lo			
+		self._la=la			#si lo et la sont different de 0 et que r est egale a 0 alors cest un rectangle lo: longeur , la: largeur
