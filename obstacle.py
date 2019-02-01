2031a01b5c94730f0b8e2ab96b7597b7fc3d8eaa
dyroxe <42959400+dyroxe@users.noreply.github.com>
2019-02-01 16:18:21 +0100

    modification du type

--- obstacle.py
+++ obstacle.py
@@ -1,11 +1,19 @@
 class Obstacle :
 	
-	def __init__(self,x,y,z,r,lo,la):
-		self._x=x
-		self._y=y
-		self._z=z
-		self._r=r				#si r est different de 0 alors cest un cercle sinon autre
-		self._lo=lo			
-		self._la=la
+	def __init__(self,type,x,y,z,r,lo,la):
+		self._type=type
+		if self._type == 'O':
+			self._x=x
+			self._y=y
+			self._z=z
+			self._r=r		#si r est different de 0 alors cest un cercle sinon autre
 		
-		#si lo et la sont different de 0 et que r est egale a 0 alors cest un rectangle lo: longeur , la: largeur
+		if self._type == 'C' :
+			self._x=x
+			self._y=y
+			self._z=z
+			self._r=r
+			self._lo=lo			
+			self._la=la
+		
+					#si lo et la sont different de 0 et que r est egale a 0 alors cest un rectangle lo: longeur , la: largeur
