116b17ece0638e1d59614666fd66a6f1bd8c44a4
vicvance188 <46991759+vicvance188@users.noreply.github.com>
2019-02-04 16:50:52 +0100

    affichage robot (initialisation)

--- View.py
+++ View.py
@@ -1,14 +1,17 @@
 from tkinter import *
-from Arene import *
+from Robot import *
 
 class View:
 	def __init__(self,x=800, y=600):
-		"""intxint -> View"""
-		self._x = x
-		self._y = y
-		fenetre = Tk()
-		canvas = Canvas(fenetre, width=x, height=y, background='white')
-		canvas.pack()
+		"""int * int -> View"""
+		self._fenetre = Tk()
+		self._canvas = Canvas(self._fenetre, width = x, height = y, background = "grey")
+		self._canvas.pack()
+		self._Bouton_Quitter = Button(self._fenetre, text = "Quitter", command = self._fenetre.destroy)
+		self._Bouton_Quitter.pack()
 
-	def end_view():
-		fenetre.mainloop()
+	def afficher_robot(self, x, y, dx, dy):
+		self._robot = self._canvas.create_polygon(x+40*dx,y+40*dy,x+10*dy,y-10*dx,x-10*dy,y+10*dx)
+
+	def endView(self):
+		self._fenetre.mainloop()
