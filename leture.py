78a6ace18052945a7059284828f2a14ac17ccd6c
dyroxe <42959400+dyroxe@users.noreply.github.com>
2019-02-04 21:32:32 +0100

    Update leture.py

--- leture.py
+++ leture.py
@@ -15,19 +15,20 @@ def lecture(fichier):
   txt=Fichier.readline()
   z=int(txt)
   
+  a= Arene(x,y,z)  #creation de arene
+  
   txt=Fichier.readline()
   n=0
-  while txt :
-    arg=txt.split("")
-    switch (arg[0]) {
-      case 'O' : ajout_Obstacle(Obstacle(int(arg[1]),int(arg[2]),int(arg[3]),float(arg[4])))
-                 break      
-      
-      case 'R' : ajout_Robot(Robot(int(arg[1]),int(arg[2]),int(arg[3]),float(arg[4]),float(arg[5]),float(arg[6]),float(arg[7]),float(arg[8]),float(arg[9]),float(arg[10]),float(arg[11]),float(arg[12])))
-                 break
-      case 'C' :  ajout_Obstacle(Obstacle(int(arg[1]),int(arg[2]),int(arg[3]),float(arg[4]),float(arg[5]),float(arg[6])))
-                 break                           
-    }  
+  while txt :    #parcour le fichier pour cree des obstacles ou robots
+    arg=txt.split(" ")
+    if 'O'==arg[0] : ajout_Obstacle(Obstacle(int(arg[1]),int(arg[2]),int(arg[3]),float(arg[4])))
+                       break        
+    if 'R'==arg[0] : ajout_Robot(Robot(int(arg[1]),int(arg[2]),int(arg[3]),float(arg[4]),float(arg[5]),float(arg[6]),float(arg[7]),float(arg[8]),float(arg[9]),float(arg[10]),float(arg[11]),float(arg[12])))
+                       break
+    if 'C'==arg[0] : ajout_Obstacle(Obstacle(int(arg[1]),int(arg[2]),int(arg[3]),float(arg[4]),float(arg[5]),float(arg[6])))
+                       break                             
     txt=Fichier.readline()
+  
   Fichier.close()    
   
+return a
