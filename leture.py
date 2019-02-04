f95a1688674234bfe9917507847001e6ab6da9ba
dyroxe <42959400+dyroxe@users.noreply.github.com>
2019-02-04 21:39:40 +0100

    modification de robot par rapport au derniere modification de la classe robot

--- leture.py
+++ leture.py
@@ -21,11 +21,11 @@ def lecture(fichier):
   n=0
   while txt :    #parcour le fichier pour cree des obstacles ou robots
     arg=txt.split(" ")
-    if 'O'==arg[0] : ajout_Obstacle(Obstacle(int(arg[1]),int(arg[2]),int(arg[3]),float(arg[4])))
+    if 'O'==arg[0] : ajout_Obstacle(Obstacle(int(arg[1]),int(arg[2]),int(arg[3]),arg[0],flot(arg[4])))
                        break        
-    if 'R'==arg[0] : ajout_Robot(Robot(int(arg[1]),int(arg[2]),int(arg[3]),float(arg[4]),float(arg[5]),float(arg[6]),float(arg[7]),float(arg[8]),float(arg[9]),float(arg[10]),float(arg[11]),float(arg[12])))
+    if 'R'==arg[0] : ajout_Robot(Robot(int(arg[1]),int(arg[2]),int(arg[3]),float(arg[4]),float(arg[5]),float(arg[6]),float(arg[7]),float(arg[8])))
                        break
-    if 'C'==arg[0] : ajout_Obstacle(Obstacle(int(arg[1]),int(arg[2]),int(arg[3]),float(arg[4]),float(arg[5]),float(arg[6])))
+    if 'C'==arg[0] : ajout_Obstacle(Obstacle(int(arg[1]),int(arg[2]),int(arg[3]),float(arg[0]),float(arg[4]),float(arg[5]),float(arg[6])))
                        break                             
     txt=Fichier.readline()
   
