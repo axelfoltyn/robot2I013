a21db2cf5c00f145fcf0708fdb347675556a37a9
vicvance188 <46991759+vicvance188@users.noreply.github.com>
2019-02-04 16:44:22 +0100

    utilisation de scalaires

--- Robot.py
+++ Robot.py
@@ -1,40 +1,28 @@
-#tous les attribus sont protected
 import math
-import numpy as np
+import random
 
 class Robot:
 
-    def __init__(self,position=[0.0,0.0,0.0],acceleration=[0.0,0.0,0.0],vitesse=[0.0,0.0,0.0],direction=[1.0,0.0,0.0]):
+    def __init__(self,position=[75.0,60.0,0.0],acceleration=0.0,vitesse=0.0,direction=[1.0,0.0,0.0]):
         self._position=position
         self._acceleration=acceleration
         self._vitesse=vitesse
         self._direction=direction
 
 
-    #a mettre dans la classe robot
     def val_accelerometre(self):
-        MIN = 0
-        MAX = 3
-        res = [0,1,2]#self.acceleration;
-        r= np.random.rand(3)
-        res[0] += r[0] * (MAX-MIN) + MIN
-        res[1] += r[1] * (MAX-MIN) + MIN
-        res[2] += r[2] * (MAX-MIN) + MIN
+        res = self._acceleration
+        res += random.random()*0.02-0.01
         return res
 
     def update(self, dt = 1):
-        self._acceleration[0] = self._acceleration[0] * self._direction[0]
-        self._acceleration[1] = self._acceleration[1] * self._direction[1]
-        self._acceleration[2] = self._acceleration[2] * self._direction[2]
-        self._vitesse[0] = self._vitesse[0] * self._direction[0] + dt * self._acceleration[0]
-        self._vitesse[1] = self._vitesse[1] * self._direction[1] + dt * self._acceleration[1]
-        self._vitesse[2] = self._vitesse[2] * self._direction[2] + dt * self._acceleration[2]
-        self._position[0] = self._position[0] * self._direction[0] + dt * self._vitesse[0]
-        self._position[1] = self._position[1] * self._direction[1] + dt * self._vitesse[1]
-        self._position[2] = self._position[2] * self._direction[2] + dt * self._vitesse[2]
+        self._vitesse += dt * self._acceleration
+        self._position[0] += dt * self._vitesse * self._direction[0]
+        self._position[1] += dt * self._vitesse * self._direction[1]
+        self._position[2] += dt * self._vitesse * self._direction[2]
 
     def tourner(self, teta=90):
-        # la rotation s'effectue vers la gauche
+        # rotation dans le sens trigo
         trad = math.radians(teta)
         dx = self._direction[0]
         dy = self._direction[1]
@@ -42,7 +30,5 @@ class Robot:
         self._direction[1] = dx*math.sin(trad) + dy*math.sin(trad)
         
     def acceleration(self,acceleration):
-        self._acceleration[0]+=acceleration*self._direction[0]
-        self._acceleration[1]+=acceleration*self._direction[1]
-        self._acceleration[2]+=acceleration*self._direction[2]
+        self._acceleration += acceleration
 
