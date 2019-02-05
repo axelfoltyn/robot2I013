faf034c967781db511ba6c10e0ad55a48116cb7b
dyroxe <42959400+dyroxe@users.noreply.github.com>
2019-02-05 18:51:20 +0100

    ajout de liste

--- lecture.py
+++ lecture.py
@@ -28,7 +28,7 @@ def lecture(fichier):
       a.ajout_Obstacle(Obstacle(int(arg[1]),int(arg[2]),int(arg[3]),arg[0],flot(arg[4])))
                             
     if 'R'==arg[0] : 
-      a.ajout_Robot(Robot(int(arg[1]),int(arg[2]),int(arg[3]),float(arg[4]),float(arg[5]),float(arg[6]),float(arg[7]),float(arg[8])))
+      a.ajout_Robot(Robot([int(arg[1]),int(arg[2]),int(arg[3])],float(arg[4]),float(arg[5]),[float(arg[6]),float(arg[7]),float(arg[8])]))
                       
     if 'C'==arg[0] : 
       a.ajout_Obstacle(Obstacle(int(arg[1]),int(arg[2]),int(arg[3]),float(arg[0]),float(arg[4]),float(arg[5]),float(arg[6])))
