a85055705b23ba2dacff50c803f7f6e0401ffc65
vicvance188 <46991759+vicvance188@users.noreply.github.com>
2019-02-05 19:14:48 +0100

    Update View.py

--- View.py
+++ View.py
@@ -15,7 +15,7 @@ class View:
 		x = robot._position[0]
 		y = self._y-robot._position[1]
 		dx = robot._direction[0]
-		dy = robot._direction[1]
+		dy = -robot._direction[1]
 		self._objets.append(self._canvas.create_polygon(x+40*dx,y+40*dy,x+10*dy,y-10*dx,x-10*dy,y+10*dx))
 
 	def afficher_obstacle(self,obstacle):
