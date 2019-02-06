4c784b18853976339780b563f36d46dab5c10e01
vicvance188 <46991759+vicvance188@users.noreply.github.com>
2019-02-06 17:14:24 +0100

    correction tourner

--- Robot.py
+++ Robot.py
@@ -39,7 +39,7 @@ class Robot:
         dx = self._direction[0]
         dy = self._direction[1]
         self._direction[0] = dx*math.cos(trad) - dy*math.sin(trad)
-        self._direction[1] = dx*math.sin(trad) + dy*math.sin(trad)
+        self._direction[1] = dx*math.sin(trad) + dy*math.cos(trad)
         
     def acceleration(self,acceleration):
         self._acceleration += acceleration
