038e14050e73d3b2bde254f5c5cdd74150d8e19e
Axel Foltyn <axel.foltyn@epita.fr>
2019-02-06 16:34:38 +0100

    correction + amelioration de l'update

--- View.py
+++ View.py
@@ -1,38 +1,38 @@
 from tkinter import *
 import time
 class View:
-	def __init__(self,x=800, y=600):
-		"""int * int -> View"""
-		self._x = x
-		self._y = y
-		self._fenetre = Tk()
-		self._canvas = Canvas(self._fenetre, width = x, height = y, background = "grey")
-		self._canvas.pack()
-		self._Bouton_Quitter = Button(self._fenetre, text = "Quitter", command = self._fenetre.destroy)
-		self._Bouton_Quitter.pack()
-		self._objets = []
+        def __init__(self,x=800, y=600):
+                """int * int -> View"""
+                self._x = x
+                self._y = y
+                self._fenetre = Tk()
+                self._canvas = Canvas(self._fenetre, width = x, height = y, background = "grey")
+                self._canvas.pack()
+                self._Bouton_Quitter = Button(self._fenetre, text = "Quitter", command = self._fenetre.destroy)
+                self._Bouton_Quitter.pack()
+                self._objets = []
 
-	def afficher_robot(self,robot):
-		x = robot._position[0]
-		y = self._y-robot._position[1]
-		dx = robot._direction[0]
-		dy = -robot._direction[1]
-		self._objets.append(self._canvas.create_polygon(x+40*dx,y+40*dy,x+10*dy,y-10*dx,x-10*dy,y+10*dx))
+        def afficher_robot(self,robot):
+                x = robot._position[0]
+                y = self._y-robot._position[1]
+                dx = robot._direction[0]
+                dy = -robot._direction[1]
+                self._objets.append(self._canvas.create_polygon(x+40*dx,y+40*dy,x+10*dy,y-10*dx,x-10*dy,y+10*dx))
 
-	def afficher_obstacle(self,obstacle):
-		pass
-	### faire attention à coordonnée y qui vaudra self._y - y !!! (pour avoir affichage à l'endroit)
+        def afficher_obstacle(self,obstacle):
+                pass
+        ### faire attention à coordonnée y qui vaudra self._y - y !!! (pour avoir affichage à l'endroit)
 
-	def clear(self):
-		for e in self._objets:
-			self._canvas.delete(e)
-		self._objets = []
+        def clear(self):
+                for e in self._objets:
+                        self._canvas.delete(e)
+                self._objets = []
 
-	def endView(self):
-		self._fenetre.mainloop()
+        def endView(self):
+                self._fenetre.mainloop()
 
         #dt en s
-	def update(self, dt=1):
+        def update(self, dt=1):
                 self._canvas.pack()
                 time.sleep(dt)
                 self._canvas.update()
