bd3d66d3c9af5bfcb5de53c43d68c2d77122dc46
axelfoltyn <axel.foltyn@epita.fr>
2019-02-06 16:50:54 +0100

    changer pour utiliser la methode update

--- script.py
+++ script.py
@@ -10,7 +10,7 @@ text="resources/fichier_test.txt"
 # Test lecture de fichier et initialisation de l'arene
 arene = lecture(text)
 
-arene._robot.update()
+arene.update()
 print("Mon acceleration est : ", arene._robot._acceleration)
 print("Ma position en x est : ",arene._robot._position[0])
 print("Ma position en y est : ", arene._robot._position[1])
@@ -18,7 +18,7 @@ print("Ma position en z est : ", arene._robot._position[2])
 print("Ma direction actuelle est : " , arene._robot._direction)
 arene.afficher()
 
-arene._view.update()
+arene.update()
 
 
 print("tourner")
@@ -31,7 +31,7 @@ print("Ma position en z est : " , arene._robot._position[2])
 print("Ma direction actuelle est : " , arene._robot._direction)
 arene.afficher()
 
-arene._view.update()
+arene.update()
 
 print("recup val acc")
 print("lacceleration est de", arene._robot.val_accelerometre())
@@ -43,7 +43,7 @@ print("Ma position en z est : " , arene._robot._position[2])
 print("Ma direction actuelle est : " , arene._robot._direction)
 arene.afficher()
 
-arene._view.update()
+arene.update()
 
 print("tourner")
 arene._robot.tourner()
