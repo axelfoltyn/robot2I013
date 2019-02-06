dbbe6448e7533807601341466e92b38376c273cd
SophiaNdr <46903185+SophiaNdr@users.noreply.github.com>
2019-02-06 15:43:37 +0100

    Le nouveau script

--- script.py
+++ script.py
@@ -3,59 +3,81 @@ from View import *
 from Arene import *
 from Obstacle import *
 from lecture import *
+from Tkinter import *
+import time
 
 text="resources/fichier_test.txt"
 # Test lecture de fichier et initialisation de l'arene
 arene = lecture(text)
 
 arene._robot.update()
-print("Mon accéleration est : ", arene._robot._acceleration)
+print("Mon acceleration est : ", arene._robot._acceleration)
 print("Ma position en x est : ",arene._robot._position[0])
 print("Ma position en y est : ", arene._robot._position[1])
 print("Ma position en z est : ", arene._robot._position[2])
 print("Ma direction actuelle est : " , arene._robot._direction)
 arene.afficher()
 
+arene._view._canvas.pack()
+time.sleep(1)
+arene._view._canvas.update()
+
+
 print("tourner")
 arene._robot.tourner(50)
 arene._robot.update()
-print("Mon accéleration est : " , arene._robot._acceleration)
+print("Mon acceleration est : " , arene._robot._acceleration)
 print("Ma position en x est : " , arene._robot._position[0])
 print("Ma position en y est : " , arene._robot._position[1])
 print("Ma position en z est : " , arene._robot._position[2])
 print("Ma direction actuelle est : " , arene._robot._direction)
 arene.afficher()
+
+arene._view._canvas.pack()
+time.sleep(1)
+arene._view._canvas.update()
 
 print("recup val acc")
 print("lacceleration est de", arene._robot.val_accelerometre())
 arene._robot.update()
-print("Mon accéleration est : " , arene._robot._acceleration)
+print("Mon acceleration est : " , arene._robot._acceleration)
 print("Ma position en x est : " , arene._robot._position[0])
 print("Ma position en y est : " , arene._robot._position[1])
 print("Ma position en z est : " , arene._robot._position[2])
 print("Ma direction actuelle est : " , arene._robot._direction)
 arene.afficher()
+
+arene._view._canvas.pack()
+time.sleep(1)
+arene._view._canvas.update()
 
 print("tourner")
 arene._robot.tourner()
 arene._robot.update()
-print("Mon accéleration est : " , arene._robot._acceleration)
+print("Mon acceleration est : " , arene._robot._acceleration)
 print("Ma position en x est : " , arene._robot._position[0])
 print("Ma position en y est : " , arene._robot._position[1])
 print("Ma position en z est : " , arene._robot._position[2])
 print("Ma direction actuelle est : " , arene._robot._direction)
 arene.afficher()
 
+arene._view._canvas.pack()
+time.sleep(1)
+arene._view._canvas.update()
+
 print("accelerer")
-arene._robot.acceleration(1)
+arene._robot.acceleration(10)
 arene._robot.update()
 print("Mon accéleration est : " , arene._robot._acceleration)
 print("Ma position en x est : " , arene._robot._position[0])
 print("Ma position en y est : " , arene._robot._position[1])
 print("Ma position en z est : " , arene._robot._position[2])
 print("Ma direction actuelle est : " , arene._robot._direction)
-
 arene.afficher()
+
+arene._view._canvas.pack()
+time.sleep(1)
+arene._view._canvas.update()
 
 arene._view.endView()
 
