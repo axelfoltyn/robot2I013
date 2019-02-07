015f8b3d436154eadd95f8e6e74de9266e26839c
axelfoltyn <axel.foltyn@epita.fr>
2019-02-07 08:34:11 +0100

    enlever les mauvais commentaire

--- Obstacle.py
+++ Obstacle.py
@@ -1,12 +1,10 @@
 class Obstacle :
-	
+	#les verification ce font par rapport au nom O =Obstacle rond C= Obstacle Carré
 	def __init__(self,x,y,z=10,nom='O',r=1.0,lo=0.0,la=0.0):
 		self._nom=nom
 		self._x=x
 		self._y=y
 		self._z=z
-		self._r=r		#si r est different de 0 alors cest un cercle sinon autre
+		self._r=r
 		self._lo=lo			
 		self._la=la
-		
-					#si lo et la sont different de 0 et que r est egale a 0 alors cest un rectangle lo: longeur , la: largeur
