8d6b809f6c30f7f9ce4110d61cb1a03ed96893c5
SophiaNdr <46903185+SophiaNdr@users.noreply.github.com>
2019-02-11 18:46:49 +0100

    Update Obstacle.py

--- Obstacle.py
+++ Obstacle.py
@@ -1,3 +1,5 @@
+import math
+
 class Obstacle :
 	#les verification ce font par rapport au nom O =Obstacle rond C= Obstacle Carré
 	def __init__(self,x,y,z=10,nom='O',r=1.0,lo=0.0,la=0.0):
@@ -8,3 +10,13 @@ class Obstacle :
 		self._r=r
 		self._lo=lo			
 		self._la=la
+		
+	def est_dans_cercle(self,x,y,z,r):
+    		""" Retourne True si le point (x,y) est dans le cercle """
+
+    		distance = math.sqrt((self._x-x)^2+(self._y-y)^2+(self._z-z)^2)
+
+
+    		if(distance<=r):
+        		return True
+
