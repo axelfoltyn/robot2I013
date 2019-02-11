ebf4d56e2a12cdb02afab727f7cc453702a15ecd
dyroxe <42959400+dyroxe@users.noreply.github.com>
2019-02-11 21:16:59 +0100

    Update Obstacle.py

--- Obstacle.py
+++ Obstacle.py
@@ -20,3 +20,12 @@ class Obstacle :
     		if(distance<=r):
         		return True
 
+	def est_dans_rectangle(self,x,y,z):
+    		x1=self._x + self._lo/2
+    		x2=self._x - self._lo/2
+    		y1=self._y + self._la/2
+    		y2=self._y - self._la/2
+    		if ( x<=x1 and x>= x2 and y<=y1 and y>=y2) :
+        		return True
+    		else:
+        		return False
