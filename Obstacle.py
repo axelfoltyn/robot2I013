755b9e52df5c12c8f0e3f86244ef15d30db93d7e
dyroxe <42959400+dyroxe@users.noreply.github.com>
2019-02-12 13:31:32 +0100

    cree la fonctionn est

--- Obstacle.py
+++ Obstacle.py
@@ -11,23 +11,22 @@ class Obstacle :
 		self._lo=lo			
 		self._la=la
 		
-	def est_dans_cercle(self,x,y,z,r):
-    		""" Retourne True si le point (x,y) est dans le cercle """
 
-    		distance = math.sqrt((self._x-x)^2+(self._y-y)^2+(self._z-z)^2)
+	def est_dans(self,x,y,z):
+		if (self._nom=='C'):	
+    			x1=self._x + self._lo/2
+    			x2=self._x - self._lo/2
+    			y1=self._y + self._la/2
+    			y2=self._y - self._la/2
+    			if ( x<=x1 and x>= x2 and y<=y1 and y>=y2) :
+        			return True
+    			else:
+        			return False
+		if (self._nom=='O'):
+			distance = math.sqrt((self._x-x)^2+(self._y-y)^2+(self._z-z)^2)
 
-
-    		if(distance<=r):
-        		return True
-		else:
-			return False
-
-	def est_dans_rectangle(self,x,y,z):
-    		x1=self._x + self._lo/2
-    		x2=self._x - self._lo/2
-    		y1=self._y + self._la/2
-    		y2=self._y - self._la/2
-    		if ( x<=x1 and x>= x2 and y<=y1 and y>=y2) :
-        		return True
-    		else:
-        		return False
+    			if(distance<=self._r):
+        			return True
+			else:
+				return False
+			
