49b7053345e10b8fec57b9f24327e0413627c20e
dyroxe <42959400+dyroxe@users.noreply.github.com>
2019-02-13 16:46:07 +0100

    ajout de ** pour les puissances

--- Obstacle.py
+++ Obstacle.py
@@ -9,7 +9,6 @@ class Obstacle:
                 self._r=r
                 self._lo=lo
                 self._la=la
-		
         def est_dans(self,x,y,z):
                 if (self._nom=='C'):
                         x1=self._x + self._lo/2
@@ -21,9 +20,9 @@ class Obstacle:
                         else:
                                 return False
                 if (self._nom=='O'):
-                        distance = math.sqrt((self._x-x)^2+(self._y-y)^2+(self._z-z)^2)
+                        distance = math.sqrt((self._x-x)**2+(self._y-y)**2+(self._z-z)**2)
+
                         if(distance<=self._r):
                                 return True
                         else:
                                 return False
-
