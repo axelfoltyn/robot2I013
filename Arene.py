b67f62e5b44a93b3b944c0c0ba098cabc56c8ec5
axelfoltyn <axel.foltyn@epita.fr>
2019-02-13 16:43:33 +0100

    Update Arene.py

--- Arene.py
+++ Arene.py
@@ -49,7 +49,7 @@ class Arene:
         while(res < max):
             if (x_p + self._robot._direction[0]*res < 0 or x_p + self._robot._direction[0]*res > self._x or y_p + self._robot._direction[1]*res < 0 or y_p + self._robot._direction[1]*res > self._y):
                 return res
-            for o in obstacles:
+            for o in self._obstacles:
                 if o.est_dans(x_p + self._robot._direction[0]*res, y_p + self._robot._direction[1]*res, z_p + self._robot._direction[2]*res):
                     return res
             res += 0.1
