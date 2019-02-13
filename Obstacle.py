68cd4a92c65ee326e987dbff25aa7bae7367cf77
AhmedGalela <46992367+AhmedGalela@users.noreply.github.com>
2019-02-13 22:58:37 +0100

    Update Obstacle.py
    Commentaires ajoutés

--- Obstacle.py
+++ Obstacle.py
@@ -1,8 +1,8 @@
 import math
 
 class Obstacle :
-  #les verification ce font par rapport au nom O =Obstacle rond C= Obstacle Carré
-  def __init__(self,x,y,z=10,nom='O',r=1.0,lo=0.0,la=0.0):
+  #les verifications se font par rapport au nom O =Obstacle rond C= Obstacle Carré
+  def __init__(self,x,y,z=10,nom='O',r=1.0,lo=0.0,la=0.0):                       #Initialisation des arguments
     self._nom=nom
     self._x=x
     self._y=y
@@ -11,19 +11,19 @@ class Obstacle :
     self._lo=lo
     self._la=la
 
-  def est_dans(self,x,y,z):
-    if (self._nom=='C'):
-      x1=self._x
+  def est_dans(self,x,y,z):                                                     #On vérifie si l'objet de coordonées x,y,z se trouve dans l'obstacle
+    if (self._nom=='C'):                                                        #Si c'est un rectangle
+      x1=self._x                                                                #x1 ,x2 , y1 et y2 représentent les quatres points du rcetangle(Carre)
       x2=self._x + self._lo
       y1=self._y
       y2=self._y - self._la
       print(self._nom,"||", x1,x,x2,"||",y1,y,y2)
-      if ( x>=x1 and x<= x2 and y<=y1 and y>=y2) :
+      if ( x>=x1 and x<= x2 and y<=y1 and y>=y2) :                              #Si les coordonées de l'objet touchent le rectangle ou sont dedans
         return True
-      else:
+      else:                                                                     #Sinon
         return False
-    if (self._nom=='O'):
-      distance = math.sqrt((self._x-x)**2+(self._y-y)**2+(self._z-z)**2)
+    if (self._nom=='O'):                                                        #Pour un obstacle Rond
+      distance = math.sqrt((self._x-x)**2+(self._y-y)**2+(self._z-z)**2)        #On verifie si les coordonées de l'objet se trouvent dans le cercle
       if(distance<=self._r):
         return True
       else:
