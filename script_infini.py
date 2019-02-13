59b2f6ff01dccd05d010f8592f21898f4b6fdd96
dyroxe <42959400+dyroxe@users.noreply.github.com>
2019-02-13 19:02:55 +0100

    enleve les prints

--- script_infini.py
+++ script_infini.py
@@ -12,17 +12,11 @@ text="resources/fichier_test.txt"
 arene = lecture(text)
 
 while (True):
-  print("Ma distance est : " , arene.proximite())
   while (arene.proximite() <= 55 ):
     arene._robot._vitesse=0.0
     arene._robot._acceleration=0.0
-    print("tourne")
     arene._robot.tourner(randint(1, 360))
-    print("lalala")
-    #arene.update(0.40)
-    print("Ma distance est : " , arene.proximite())
   if (arene._robot._acceleration==0.0):
     arene._robot.acceleration(10)
-    print("lblblb")
   arene.afficher()
   arene.update(0.40)
