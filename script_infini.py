d6488b74958d2f86d7db1758de3bf0f88968f82c
SALIEN <3700244@ppti-14-305-04.ufr-info-p6.jussieu.fr>
2019-02-14 17:22:09 +0100

    ajout de stop

--- script_infini.py
+++ script_infini.py
@@ -13,8 +13,7 @@ arene = lecture(text)
 
 while (True):                                         #On crée une boucle infinie
   while (arene.proximite() <= 55 ):                   #Tant que le robot est à une distance inférieure à 55 d'un objet
-    arene._robot._vitesse=0.0                         #On met l'acceleration et la vitesse à 0
-    arene._robot._acceleration=0.0
+    arene._robot.stop()                         #On met l'acceleration et la vitesse à 0
     arene._robot.tourner(randint(1, 360))             #Le robot tourne ensuite avec un angle qui prend une valeur aléatoire entre 0° et 360°
   if (arene._robot._acceleration==0.0):               #Si l'accélartion a été mise à 0
     arene._robot.acceleration(10)                     #On la remet à 10 l'accéleration pour que le robot reparte
