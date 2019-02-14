0e587003cf35962f39179b3eaa4252c66c945ea6
vicvance188 <vic188@hotmail.fr>
2019-02-14 01:04:02 +0100

    modif estDans

--- Obstacle.py
+++ Obstacle.py
@@ -1,7 +1,7 @@
 import math
 
 class Obstacle :
-  #les verifications se font par rapport au nom O =Obstacle rond C= Obstacle Carré
+  #les verifications se font par rapport au nom O = Obstacle rond et C = Obstacle Carre
   def __init__(self,x,y,z=10,nom='O',r=1.0,lo=0.0,la=0.0):                       #Initialisation des arguments
     self._nom=nom
     self._x=x
@@ -17,14 +17,6 @@ class Obstacle :
       x2=self._x + self._lo
       y1=self._y
       y2=self._y - self._la
-      print(self._nom,"||", x1,x,x2,"||",y1,y,y2)
-      if ( x>=x1 and x<= x2 and y<=y1 and y>=y2) :                              #Si les coordonées de l'objet touchent le rectangle ou sont dedans
-        return True
-      else:                                                                     #Sinon
-        return False
+      return x>=x1 and x<= x2 and y<=y1 and y>=y2         #Si les coordonées de l'objet touchent le rectangle ou sont dedans
     if (self._nom=='O'):                                                        #Pour un obstacle Rond
-      distance = math.sqrt((self._x-x)**2+(self._y-y)**2+(self._z-z)**2)        #On verifie si les coordonées de l'objet se trouvent dans le cercle
-      if(distance<=self._r):
-        return True
-      else:
-        return False
+      return (x-self._x)**2+(y-self._y)**2 <= self._r**2    #On verifie si les coordonées de l'objet se trouvent dans le cercle
