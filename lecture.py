02818229a426894a5c58246c1ea7fcc73a5cc6b7
vicvance188 <vic188@hotmail.fr>
2019-02-14 12:10:12 +0100

    mise aux normes tabulations

--- lecture.py
+++ lecture.py
@@ -7,32 +7,22 @@ from obstacle import *
 from robot import *
 
 def lecture(fichier):
-  Fichier=open(fichier,"r")
-   
-  txt=Fichier.readline()
-  x=int(txt)
-  
-  txt=Fichier.readline()
-  y=int(txt)
-  
-  txt=Fichier.readline()
-  z=int(txt)
-  
-  a= Arene(x,y,z)  #creation de arene
-  
-  n=0
-  for elt in Fichier.readlines() :    #parcour le fichier pour cree des obstacles ou robots
-    arg=elt.split(" ")
-    if 'O'==arg[0] : 
-      a.ajout_Obstacle(Obstacle(int(arg[1]),int(arg[2]),int(arg[3]),arg[0],float(arg[4])))
-                            
-    if 'R'==arg[0] : 
-      a.ajout_Robot(Robot([int(arg[1]),int(arg[2]),int(arg[3])],float(arg[4]),float(arg[5]),[float(arg[6]),float(arg[7]),float(arg[8])]))
-                      
-    if 'C'==arg[0] :
-      a.ajout_Obstacle(Obstacle(int(arg[1]),int(arg[2]),int(arg[3]),arg[0],lo=float(arg[4]),la=float(arg[5])))
-                                                   
-      
-  Fichier.close()    
-  
-  return a
+    Fichier=open(fichier,"r")
+    txt=Fichier.readline()
+    x=int(txt)
+    txt=Fichier.readline()
+    y=int(txt)
+    txt=Fichier.readline()
+    z=int(txt)
+    a=Arene(x,y,z)                      #creation de arene
+    n=0
+    for elt in Fichier.readlines():     #parcour le fichier pour cree des obstacles ou robots
+        arg=elt.split(" ")
+        if 'O'==arg[0] :
+            a.ajout_Obstacle(Obstacle(int(arg[1]),int(arg[2]),int(arg[3]),arg[0],float(arg[4])))
+        if 'R'==arg[0] : 
+            a.ajout_Robot(Robot([int(arg[1]),int(arg[2]),int(arg[3])],float(arg[4]),float(arg[5]),[float(arg[6]),float(arg[7]),float(arg[8])]))
+        if 'C'==arg[0] :
+            a.ajout_Obstacle(Obstacle(int(arg[1]),int(arg[2]),int(arg[3]),arg[0],lo=float(arg[4]),la=float(arg[5])))
+    Fichier.close()
+    return a
