1070a06c39ea33720eb4d5cc7fe98b2918917083
AhmedGalela <46992367+AhmedGalela@users.noreply.github.com>
2019-02-20 22:54:39 +0100

    Update robot.py

--- robot.py
+++ robot.py
@@ -8,7 +8,13 @@ class Robot:
 
 
     def __init__(self,position=[75,60,0],acceleration=0.0,vitesse=0.0,direction=[1.0,0.0,0.0]):
-        self._position=position
+        """Cette fonction inotialise un robot 
+        : param position     : position du robot 
+        : param acceleration : acceleration du robot 
+        : param vitesse      : vitesse du robot 
+        : param direction    : direction du robot 
+        """
+        self._position=position              # initialisation des parametres du robot 
         self._acceleration=acceleration
         self._vitesse=vitesse
         self._direction=direction
@@ -18,6 +24,9 @@ class Robot:
         self._max_bruit_proximite = 1.0
 
     def val_accelerometre(self):
+        """Cette  fonction renvoie la valeur de l'accelerometre en prenant en compte 
+           le bruit éventuelle. 
+        """
         MIN = self._min_bruit_acceleration
         MAX = self._max_bruit_acceleration
         res = [self._acceleration*self._direction[0], self._acceleration*self._direction[1], self._acceleration * self._direction[2]];
@@ -28,12 +37,19 @@ class Robot:
         return res
 
     def update(self, dt = 1):
+        """Cette fonction met à jour la position et la vitesse du robot 
+           en fonction de sa vitesse et de on acceleration
+        """
         self._vitesse += dt * self._acceleration
         self._position[0] += dt * self._vitesse * self._direction[0]
         self._position[1] += dt * self._vitesse * self._direction[1]
         self._position[2] += dt * self._vitesse * self._direction[2]
 
     def tourner(self, teta=90):
+        """
+        Cette fonction fait tourner le robot d'un angle teta qui est à 90° par défaut
+        : param teta : angle de rotation 
+        """
         # rotation dans le sens trigo
         trad = math.radians(teta)
         dx = self._direction[0]
@@ -42,13 +58,23 @@ class Robot:
         self._direction[1] = dx*math.sin(trad) + dy*math.cos(trad)
 
     def acceleration(self,acceleration):
+        """
+        Cette fonction fait accélérer le robot 
+        : param acceleration : valeur de l'acceleration que l'on souhaite ajouter à l'accelaration courante
+        """
             self._acceleration += acceleration      # if(self._acceleration <= 60):
 
     def stop(self):
+        """
+        Fonction STOP qui met l'acceleration et la vitesse du robot à 0
+        """
         self._acceleration=0
         self._vitesse=0
 
     def toString(self):
+        """
+        Retourne toutes les données du robot sous forme de chaine de caractere 
+        """
         return "R"+" "+str(self._position[0])+" "+str(self._position[1])+" "+str(self._position[2])+" "+str(self._acceleration)+" "+str(self._vitesse)+" "+str(self._direction[0])+" "+str(self._direction[1])+" "+str(self._direction[2])
 
     def proximite_bruit(self,arene):
