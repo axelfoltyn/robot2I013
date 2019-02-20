fd4e5a9cd21a0c6057e73a09e16239d7fd37ad38
Axel Foltyn <axel.foltyn@epita.fr>
2019-02-20 17:19:33 +0100

    essai de faire un package

---
+++ model/__init__.py
@@ -0,0 +1,5 @@
+from . robot import Robot
+from . arene import Arene
+from . obstacle import Obstacle, Obstacle_carre, Obstacle_rond
+
+__all__=['Robot', 'Arene', 'Obstacle', 'Obstacle_carre', 'Obstacle_rond']
