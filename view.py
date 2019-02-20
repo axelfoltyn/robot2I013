ce2d5897842f12f2c2ea63743047bfbb35f81e4d
AhmedGalela <46992367+AhmedGalela@users.noreply.github.com>
2019-02-20 23:17:22 +0100

    Update view.py
    Commentaires

--- view.py
+++ view.py
@@ -1,40 +1,55 @@
 import sys
 import time
 from tkinter import *
+from tkinter import messagebox
 from obstacle import *
 
 class View:
     def __init__(self,x=800, y=600):
-        """int * int -> View"""
-        self._x = x
-        self._y = y
-        self._fenetre = Tk()
-        self._canvas = Canvas(self._fenetre, width = x, height = y, background = "grey")
-        self._canvas.pack()
-        self._Bouton_Quitter = Button(self._fenetre, text = "Quitter", command = self._fenetre.destroy)
-        self._Bouton_Quitter.pack()
-        self._objets = []
+        """
+         int * int -> View
+         Cette fonction initialise la fenetre sur laquelle le robot va s'afficher 
+         : param x: largeur de la fenetre 
+         : param y: heuteur de la fenetre 
+         """
+         #initialisation des parametres
+         self._x = x
+         self._y = y
+         self._fenetre = Tk()
+         self._canvas = Canvas(self._fenetre, width = x, height = y, background = "grey")
+         self._canvas.pack()
+         self._Bouton_Quitter = Button(self._fenetre, text = "Quitter", command = self._fenetre.destroy) #creation du boutton quitter
+         self._Bouton_Quitter.pack()
+         self._objets = []
 
     def afficher_robot(self,robot):
-        x = robot._position[0]
-        y = self._y-robot._position[1]
-        dx = robot._direction[0]
-        dy = -robot._direction[1]
-        self._objets.append(self._canvas.create_polygon(x+40*dx,y+40*dy,x+10*dy,y-10*dx,x-10*dy,y+10*dx))
+                """
+                Cette fonction affiche le robot sur le canevas
+                : param robot : robot à afficher
+                """
+                x = robot._position[0]
+                y = self._y-robot._position[1]
+                dx = robot._direction[0]
+                dy = -robot._direction[1]
+                self._objets.append(self._canvas.create_polygon(x+40*dx,y+40*dy,x+10*dy,y-10*dx,x-10*dy,y+10*dx))
 
     def afficher_obstacle(self,obstacle):
+        """
+        Cette fonction permet d'afficher les obstacles 
+        : param obstacle : obstacle à afficher 
+        """
         x0=obstacle._x
         y0=self._y-obstacle._y
         #si r est different de 0 alors cest un cercle sinon autre
-        if isinstance(obstacle, Obstacle_carre):
+        if obstacle._nom == 'C':
             if (obstacle._lo != 0) and (obstacle._la != 0):
                 x1 = obstacle._x
                 y1 = self._y-obstacle._y
                 x2 = x1+obstacle._lo
                 y2 = y1+obstacle._la
                 self._objets.append(self._canvas.create_rectangle(x1, y1, x2, y2,fill = "black"))
-            else:
-                print("L'obstacle n'existe pas")
+             else:
+                 print("L'obstacle n'existe pas")
         else:
             x0 = obstacle._x - obstacle._r
             y0 = y0 - obstacle._r
@@ -43,19 +58,27 @@ class View:
             y1=y0+(2*r1)
             self._objets.append(self._canvas.create_oval(x0, y0, x1, y1,fill = "black"))
 
-# faire attention à coordonnée y qui vaudra self._y - y !!! (pour avoir affichage à l'endroit)
 
-    def clear(self):
-        for e in self._objets:
-            self._canvas.delete(e)
-        self._objets = []
+        ### faire attention à coordonnée y qui vaudra self._y - y !!! (pour avoir affichage à l'endroit)
+
+     def clear(self):
+                """
+                Cette fonction supprime tous les éléments affichés sur l'arene 
+                """
+                for e in self._objets:
+                        self._canvas.delete(e)
+                self._objets = []
+
+
 
 
     def end_view(self, b=True):
         if b:
             self._canvas.configure(background = "lime green")
+            messagebox.showinfo("Fin du parcours","Le parcours vient de se terminer")
         else:
             self._canvas.configure(background = "red")
+            messagebox.showwarning("Fin du parcours","Le robot s'est cogné")
         self._fenetre.mainloop()
 
     #dt en s
