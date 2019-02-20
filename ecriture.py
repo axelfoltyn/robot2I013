bdd9b0cdb287ea1bbe1b8ad04f5504344a069dab
AhmedGalela <46992367+AhmedGalela@users.noreply.github.com>
2019-02-20 22:25:02 +0100

    Update ecriture.py
    Ajout de commentaires

--- ecriture.py
+++ ecriture.py
@@ -3,13 +3,18 @@ from robot import *
 from obstacle import *
 
 def ecriture2(fichier, arene):
-    Fichier=open(fichier,"w")
-    Fichier.write(str(arene._x)+"\n")
+    """
+    Cette fonction écrit dans un fichier l'état actuelle de l'arène
+    : param fichier : fichier dans lequel on écrit 
+    : param arene : arene à sauvegarder 
+    """
+    Fichier=open(fichier,"w")                   #ouverture du fichier 
+    Fichier.write(str(arene._x)+"\n")           #coordonées de l'arène
     Fichier.write(str(arene._y)+"\n")
     Fichier.write(str(arene._z)+"\n")
 
-    for i in arene._obstacles :
+    for i in arene._obstacles :                 #sauvegarde des obstacles 
         Fichier.write(i.toString()+"\n")
 
-    Fichier.write(arene._robot.toString()+"\n")
-    Fichier.close()
+    Fichier.write(arene._robot.toString()+"\n") #sauvegarde du robot 
+    Fichier.close()                             #fermeture du fichier 
