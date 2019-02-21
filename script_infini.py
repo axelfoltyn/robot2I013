37a823446361b89a77e248330e050e666d432b26
dyroxe <sebast-94@hotmail.fr>
2019-02-21 17:15:23 +0100

    mod

--- script_infini.py
+++ script_infini.py
@@ -14,9 +14,9 @@ while (True):
     if (arene._robot._acceleration==0.0):
         arene._robot.acceleration(20)
     if (random() <= 0.10):
-        ecriture2("resources/fichier_test.txt", arene )
+        ecriture2("resources/fichier_test2.txt", arene )
     if (random()<=0.0075) : #utilise pour relire le fichier
-        arene = lecture("resources/fichier_test.txt")
+        arene = lecture("resources/fichier_test2.txt")
     arene.afficher()
     arene.update(0.40)
 
