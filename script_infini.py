fe1bd8336a0fcb0d9c8beac4c0d4500abb2cf1a2
axelfoltyn <axel.foltyn@epita.fr>
2019-02-21 16:15:40 +0100

    Update script_infini.py

--- script_infini.py
+++ script_infini.py
@@ -7,22 +7,18 @@ text="resources/fichier_test.txt"
 # Test lecture de fichier et initialisation de l'arene
 arene = lecture(text)
 
-
 while (True):
-    assert(arene.proximite() - arene._robot.proximite_bruit(arene) >= -1 and arene.proximite() - arene._robot.proximite_bruit(arene) <= 1)
     while (arene.proximite() <= 55 ):
-        assert(arene.proximite() - arene._robot.proximite_bruit(arene) >= -1 and arene.proximite() - arene._robot.proximite_bruit(arene) <= 1)
         arene._robot.stop()
         arene._robot.tourner(randint(1, 360))
     if (arene._robot._acceleration==0.0):
         arene._robot.acceleration(20)
-    if (random.random() <= 0.10):
-        ecriture2("resources/fichier_test2.txt", arene )
-    if (random.random()<=0.0075) : #utilise pour relire le fichier
-        arene = lecture("resources/fichier_test2.txt")
+    if (random() <= 0.10):
+        ecriture2("resources/fichier_test.txt", arene )
+    if (random()<=0.0075) : #utilise pour relire le fichier
+        arene = lecture("resources/fichier_test.txt")
     arene.afficher()
     arene.update(0.40)
-
 
 
 
