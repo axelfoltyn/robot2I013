b219b7d2909b1b7611b215b30692bbb555ce54de
Axel Foltyn <axel.foltyn@epita.fr>
2019-02-21 08:23:02 +0100

    update pour le rendu

--- view.py
+++ view.py
@@ -7,49 +7,49 @@ from obstacle import *
 class View:
     def __init__(self,x=800, y=600):
         """
-         int * int -> View
-         Cette fonction initialise la fenetre sur laquelle le robot va s'afficher 
-         : param x: largeur de la fenetre 
-         : param y: heuteur de la fenetre 
-         """
-         #initialisation des parametres
-         self._x = x
-         self._y = y
-         self._fenetre = Tk()
-         self._canvas = Canvas(self._fenetre, width = x, height = y, background = "grey")
-         self._canvas.pack()
-         self._Bouton_Quitter = Button(self._fenetre, text = "Quitter", command = self._fenetre.destroy) #creation du boutton quitter
-         self._Bouton_Quitter.pack()
-         self._objets = []
+        int * int -> View
+        Cette fonction initialise la fenetre sur laquelle le robot va s'afficher
+        : param x: largeur de la fenetre
+        : param y: heuteur de la fenetre
+        """
+        #initialisation des parametres
+        self._x = x
+        self._y = y
+        self._fenetre = Tk()
+        self._canvas = Canvas(self._fenetre, width = x, height = y, background = "grey")
+        self._canvas.pack()
+        self._Bouton_Quitter = Button(self._fenetre, text = "Quitter", command = self._fenetre.destroy) #creation du boutton quitter
+        self._Bouton_Quitter.pack()
+        self._objets = []
 
     def afficher_robot(self,robot):
-                """
-                Cette fonction affiche le robot sur le canevas
-                : param robot : robot à afficher
-                """
-                x = robot._position[0]
-                y = self._y-robot._position[1]
-                dx = robot._direction[0]
-                dy = -robot._direction[1]
-                self._objets.append(self._canvas.create_polygon(x+40*dx,y+40*dy,x+10*dy,y-10*dx,x-10*dy,y+10*dx))
+        """
+        Cette fonction affiche le robot sur le canevas
+        : param robot : robot à afficher
+        """
+        x = robot._position[0]
+        y = self._y-robot._position[1]
+        dx = robot._direction[0]
+        dy = -robot._direction[1]
+        self._objets.append(self._canvas.create_polygon(x+40*dx,y+40*dy,x+10*dy,y-10*dx,x-10*dy,y+10*dx))
 
     def afficher_obstacle(self,obstacle):
         """
-        Cette fonction permet d'afficher les obstacles 
-        : param obstacle : obstacle à afficher 
+        Cette fonction permet d'afficher les obstacles
+        : param obstacle : obstacle à afficher
         """
         x0=obstacle._x
         y0=self._y-obstacle._y
         #si r est different de 0 alors cest un cercle sinon autre
-        if obstacle._nom == 'C':
+        if isinstance(obstacle, Obstacle_carre):
             if (obstacle._lo != 0) and (obstacle._la != 0):
                 x1 = obstacle._x
                 y1 = self._y-obstacle._y
                 x2 = x1+obstacle._lo
                 y2 = y1+obstacle._la
                 self._objets.append(self._canvas.create_rectangle(x1, y1, x2, y2,fill = "black"))
-             else:
-                 print("L'obstacle n'existe pas")
+            else:
+                print("L'obstacle n'existe pas")
         else:
             x0 = obstacle._x - obstacle._r
             y0 = y0 - obstacle._r
@@ -63,7 +63,7 @@ class View:
 
     def clear(self):
                 """
-                Cette fonction supprime tous les éléments affichés sur l'arene 
+                Cette fonction supprime tous les éléments affichés sur l'arene
                 """
                 for e in self._objets:
                         self._canvas.delete(e)
