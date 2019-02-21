c743102fe53b50dc828687fcaad0140b1a27348b
Axel Foltyn <axel.foltyn@epita.fr>
2019-02-21 08:52:21 +0100

    update rendu

--- robot.py
+++ robot.py
@@ -23,9 +23,10 @@ class Robot:
         self._min_bruit_proximite = -1.0
         self._max_bruit_proximite = 1.0
 
+
     def val_accelerometre(self):
         """Cette  fonction renvoie la valeur de l'accelerometre en prenant en compte
-           le bruit éventuelle.
+            le bruit eventuelle.
         """
         MIN = self._min_bruit_acceleration
         MAX = self._max_bruit_acceleration
@@ -37,7 +38,7 @@ class Robot:
         return res
 
     def update(self, dt = 1):
-        """Cette fonction met à jour la position et la vitesse du robot
+        """Cette fonction met a jour la position et la vitesse du robot
            en fonction de sa vitesse et de on acceleration
         """
         self._vitesse += dt * self._acceleration
@@ -47,7 +48,7 @@ class Robot:
 
     def tourner(self, teta=90):
         """
-        Cette fonction fait tourner le robot d'un angle teta qui est à 90° par défaut
+        Cette fonction fait tourner le robot d'un angle teta qui est a 90 degre par defaut
         : param teta : angle de rotation
         """
         # rotation dans le sens trigo
@@ -59,21 +60,21 @@ class Robot:
 
     def acceleration(self,acceleration):
         """
-        Cette fonction fait accélérer le robot
-        : param acceleration : valeur de l'acceleration que l'on souhaite ajouter à l'accelaration courante
+        Cette fonction fait accelerer le robot
+        : param acceleration : valeur de l'acceleration que l'on souhaite ajouter a l'accelaration courante
         """
         self._acceleration += acceleration      # if(self._acceleration <= 60):
 
     def stop(self):
         """
-        Fonction STOP qui met l'acceleration et la vitesse du robot à 0
+        Fonction STOP qui met l'acceleration et la vitesse du robot a 0
         """
         self._acceleration=0
         self._vitesse=0
 
     def toString(self):
         """
-        Retourne toutes les données du robot sous forme de chaine de caractere
+        Retourne toutes les donnees du robot sous forme de chaine de caractere
         """
         return "R"+" "+str(self._position[0])+" "+str(self._position[1])+" "+str(self._position[2])+" "+str(self._acceleration)+" "+str(self._vitesse)+" "+str(self._direction[0])+" "+str(self._direction[1])+" "+str(self._direction[2])
 
