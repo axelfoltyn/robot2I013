bdd9e4c04420634cc1528a203b9b919b98f118f0
Axel Foltyn <axel.foltyn@epita.fr>
2019-02-26 18:54:47 +0100

    add fonction for adaptateur

--- Projet/model/adaptateur_virtuel.py
+++ Projet/model/adaptateur_virtuel.py
@@ -1,1 +1,21 @@
+class AdaptateurVirturel:
+    def __init__():
+        pass
 
+    def tourner_droite():
+        pass
+
+    def tourner_gauche():
+        pass
+
+    def Avancer():
+        pass
+
+    def stop():
+        pass
+
+    def get_proximite():
+        pass
+
+    def get_image():
+        pass
