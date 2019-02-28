4c1c881009eefc5a82fca27c0a7dba9c0d03e4f2
AhmedGalela <ahmedgalela@yahoo.fr>
2019-02-28 12:15:24 +0100

    Ajout de Nombre de tour

--- Projet/model/adaptateur_virtuel.py
+++ Projet/model/adaptateur_virtuel.py
@@ -25,11 +25,13 @@ class AdaptateurVirtuel:
 #
 #    #def get_image():
     #    pass
-    WHEEL_BASE_WIDTH         = 117  # distance (mm) de la roue gauche a la roue droite.
-    WHEEL_DIAMETER           = 66.5 #  diametre de la roue (mm)
+    WHEEL_BASE_WIDTH         = 117                        # distance (mm) de la roue gauche a la roue droite.
+    WHEEL_DIAMETER           = 66.5                       # diametre de la roue (mm)
     WHEEL_BASE_CIRCUMFERENCE = WHEEL_BASE_WIDTH * math.pi # perimetre du cercle de rotation (mm)
     WHEEL_CIRCUMFERENCE      = WHEEL_DIAMETER   * math.pi # perimetre de la roue (mm)
-    
+    NB_Tours_Gauche          = 0                          # Nombre de tour du moteur gauche  
+    NB_Tours_Droit           = 0                          # Nombre de toursdu moteur droit
+
     def __init__(self, controler, fps=25, resolution=None, servoPort="SERVO1",motionPort="AD1"):
         text="resources/fichier_test.txt"
         # Test lecture de fichier et initialisation de l'arene
