a57d6eff60d29663b4226dcac84f3f37003bd17d
AhmedGalela <ahmedgalela@yahoo.fr>
2019-02-28 11:59:27 +0100

    ajout de variables globales

--- Projet/model/adaptateur_virtuel.py
+++ Projet/model/adaptateur_virtuel.py
@@ -25,7 +25,11 @@ class AdaptateurVirtuel:
 #
 #    #def get_image():
     #    pass
-
+    WHEEL_BASE_WIDTH         = 117  # distance (mm) de la roue gauche a la roue droite.
+    WHEEL_DIAMETER           = 66.5 #  diametre de la roue (mm)
+    WHEEL_BASE_CIRCUMFERENCE = WHEEL_BASE_WIDTH * math.pi # perimetre du cercle de rotation (mm)
+    WHEEL_CIRCUMFERENCE      = WHEEL_DIAMETER   * math.pi # perimetre de la roue (mm)
+    
     def __init__(self, controler, fps=25, resolution=None, servoPort="SERVO1",motionPort="AD1"):
         text="resources/fichier_test.txt"
         # Test lecture de fichier et initialisation de l'arene
