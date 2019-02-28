abf52109985ca00959e7b280f5921b15d3052599
SophiaNdr <46903185+SophiaNdr@users.noreply.github.com>
2019-02-28 10:43:21 +0100

    Delete ia_robot.py

--- ia_robot.py
+++
@@ -1,8 +0,0 @@
-def ia_robot(arene,robot):
-    while (True):
-        if(robot.proximite_bruit(arene)<20):
-            robot.avancer(20)
-        else :
-            tourner(90)
-
-#De ce que j'ai essayé ça fonctionne
