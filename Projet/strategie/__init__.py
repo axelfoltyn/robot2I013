32ef3abe58b0df0568776fed8dbf9c44022b6600
dyroxe <sebast-94@hotmail.fr>
2019-03-07 11:01:13 +0100

    nouveau dossier

---
+++ Projet/strategie/__init__.py
@@ -0,0 +1,3 @@
+from .strategie import Strategie_avance, Strategie_tourner_droite, Strategie_tourner_gauche, Strategie_carre, Strategie_fonce
+
+__all__=["Strategie_avance", "Strategie_tourner_droite", "Strategie_tourner_gauche", "Strategie_carre", "Strategie_fonce"]
