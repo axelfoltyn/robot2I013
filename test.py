1c0c511a5075b0a386288b1af9ede5430ca84099
Axel Foltyn <axel.foltyn@epita.fr>
2019-03-07 11:25:33 +0100

    commit pour pull

--- test.py
+++ test.py
@@ -4,11 +4,10 @@ from Projet import Adapter as Robot
 
 
 robot = Robot()
+print("sjdfghsjdgf")
 strat = Strategie_fonce(robot, 10, 15)
 
 while not strat.stop():
     dt = strat.update()
-    robot.update(dt)
-    print(dt)
     time.sleep(dt)
 robot.finish()
