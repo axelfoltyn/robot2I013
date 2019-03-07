a614cd05f602c4fe6bcc86c6e4e07fe6fda497d0
vicvance188 <vic188@hotmail.fr>
2019-03-07 11:42:06 +0100

    organisation code

--- Projet/__init__.py
+++ Projet/__init__.py
@@ -1,6 +1,4 @@
 from .model import RobotVirtuel, Arene, Obstacle, Obstacle_carre, Obstacle_rond, lecture, ecriture2
 from .view import View
-from .model import AdaptateurVirtuel
-from .model import Adapter
 from .strategie import Strategie_avance, Strategie_tourner_droite, Strategie_tourner_gauche, Strategie_carre, Strategie_fonce
-__all__ = ['RobotVirtuel','Arene','Obstacle_carre','Obstacle_rond','View', 'lecture', 'ecriture2', 'Adapter']
+from .adapter import Adapter
