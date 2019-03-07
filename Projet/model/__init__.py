247228440d076381e84bb55c2dd8d075f4b49d3e
vicvance188 <vic188@hotmail.fr>
2019-03-07 11:31:22 +0100

    organisation code

--- Projet/model/__init__.py
+++ Projet/model/__init__.py
@@ -3,9 +3,6 @@ from .arene import Arene
 from .obstacle import Obstacle, Obstacle_carre, Obstacle_rond
 from .lecture import lecture
 from .ecriture import ecriture2
-from .adaptateur_virtuel import AdaptateurVirtuel
-from .adapter import Adapter
-
 
 __all__=['RobotVirtuel', 'Arene', 'Obstacle',\
- 'Obstacle_carre', 'Obstacle_rond', 'lecture', 'ecriture2', 'Adapter']
+'Obstacle_carre', 'Obstacle_rond', 'lecture', 'ecriture2']
