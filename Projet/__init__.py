1251a8dd8a2292b7297294d16c7e47ad51186d49
axelfoltyn <axel.foltyn@epita.fr>
2019-03-07 08:56:09 +0100

    Update __init__.py

--- Projet/__init__.py
+++ Projet/__init__.py
@@ -1,5 +1,5 @@
-from .model import RobotVirtuel, Arene, Obstacle, Obstacle_carre, Obstacle_rond, lecture, ecriture2, Robot2I013
+from .model import RobotVirtuel, Arene, Obstacle, Obstacle_carre, Obstacle_rond, lecture, ecriture2
 from .view import View
 from .model import AdaptateurVirtuel
 
-__all__ = ['RobotVirtuel','Arene','Obstacle_carre','Obstacle_rond','View', 'lecture', 'ecriture2', 'Robot2I013']
+__all__ = ['RobotVirtuel','Arene','Obstacle_carre','Obstacle_rond','View', 'lecture', 'ecriture2']
