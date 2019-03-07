fd19c92040279a5ef607ff5ef3d0f4b68217229e
vicvance188 <vic188@hotmail.fr>
2019-03-07 09:05:54 +0100

    correction adapter

--- Projet/model/adapter.py
+++ Projet/model/adapter.py
@@ -1,17 +1,17 @@
 from adaptateur_virtuel import RobotVirtuel as Robot
 
-class Adapter(Robot):
+class Adapter():
 
-    def __init__(self,controler,fps=25,resolution=None,servoPort = "SERVO1",motionPort="AD1"):
-        super().__init__(controler,fps,resolution,servoPort,motionPort)
+    def __init__(self):
+        self._Robot=Robot()
 
     def action(self, code, vitesse):      # code: -1=tourner_gauche 0=avancer 1=tourner_droite
         dps=(vitesse/WHEEL_CIRCUMFERENCE)*3600
         if code==0:
-            Robot.set_motor_dps(Robot.MOTOR_LEFT+Robot.MOTOR_RIGHT, dps)
+            self._Robot.set_motor_dps(self._Robot.MOTOR_LEFT+self._Robot.MOTOR_RIGHT, dps)
         else:
-            Robot.set_motor_dps(Robot.MOTOR_LEFT, -code*dps)
-            Robot.set_motor_dps(Robot.MOTOR_RIGHT, code*dps)
+            self._Robot.set_motor_dps(self._Robot.MOTOR_LEFT, -code*dps)
+            self._Robot.set_motor_dps(self._Robot.MOTOR_RIGHT, code*dps)
 
     def tourner_gauche(self,vitesse):       # vitesse en cm/s
         self.action(-1,vitesse)
@@ -23,16 +23,16 @@ class Adapter(Robot):
             self.action(0,vitesse)
 
     def get_motor_position():
-        return Robot.get_motor_position()
+        return self._Robot.get_motor_position()
 
     def get_distance():
-        return Robot.get_distance()
+        return self._Robot.get_distance()
 
     def get_image():
-        return Robot.get_image()
+        return self._Robot.get_image()
 
     def offset_motor_encoder(self, port, offset):
-        Robot.offset_motor_encoder(port, offset)
+        self._Robot.offset_motor_encoder(port, offset)
 
     def stop(self):
-        Robot.stop()
+        self._Robot.stop()
