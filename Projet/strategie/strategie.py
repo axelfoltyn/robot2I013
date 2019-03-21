a43ee43e64dcc559d4ecbaffa34c001385a74a83
dyroxe <sebast-94@hotmail.fr>
2019-03-21 10:17:16 +0100

    carre

--- Projet/strategie/strategie.py
+++ Projet/strategie/strategie.py
@@ -7,12 +7,14 @@ class Strategie_avance:
     def __init__(self, robot, distance, vitesse,fps=25):
         self._robot=robot
         self._distance=distance
-        self._t=time.time()
         self._vitesse=vitesse
 
 
     def start(self):
-        self._t=time.time()
+        print("reset :", self._robot.get_motor_position()[1])
+        self._robot.offset_motor_encoder(self._robot.MOTOR_RIGHT, self._robot.get_motor_position()[1])
+        self._robot.offset_motor_encoder(self._robot.MOTOR_LEFT, self._robot.get_motor_position()[0])
+        print("reset :", self._robot.get_motor_position()[1])
 
     def update(self):
         if(self.stop()):
@@ -21,8 +23,9 @@ class Strategie_avance:
             self._robot.avancer(self._vitesse)
 
     def stop(self):
-        dt=(time.time()-self._t)
-        return self._distance<=dt*self._vitesse
+        circonference_cm = self._robot.WHEEL_CIRCUMFERENCE/10
+        distance = self._robot.get_motor_position()[1] * circonference_cm / 360
+        return self._distance<=distance
 
 
 
@@ -34,11 +37,12 @@ class Strategie_tourner_droite:
         self._robot=robot
         self._angle=angle
         self._vitesse=vitesse
-        self._robot.offset_motor_encoder(self._robot.MOTOR_RIGHT, self._robot.get_motor_position()[1])
 
 
     def start(self):
+        print("reset2.0 :", self._robot.get_motor_position()[1])
         self._robot.offset_motor_encoder(self._robot.MOTOR_RIGHT, self._robot.get_motor_position()[1])
+        print("reset2.0 :", self._robot.get_motor_position()[1])
 
 
     def update(self):
@@ -62,7 +66,7 @@ class Strategie_tourner_gauche:
         self._vitesse=vitesse
 
     def start(self):
-        self._t=time.time()
+        self._robot.offset_motor_encoder(self._robot.MOTOR_LEFT, self._robot.get_motor_position()[0])
 
 
     def update(self):
@@ -74,8 +78,10 @@ class Strategie_tourner_gauche:
         return 1/fps
 
     def stop(self):
-        dt=(time.time()-self._t)
-        return self._angle<=dt*self._vitesse
+        circonference_cm = self._robot.WHEEL_CIRCUMFERENCE/10
+        distance = self._robot.get_motor_position()[0] * circonference_cm / 360
+        deta=distance * 360.0 / self._robot.WHEEL_BASE_CIRCUMFERENCE
+        return self._angle<=deta
 
 
 
@@ -93,27 +99,28 @@ class Strategie_carre:
 
 
     def start(self):
-        self._t=time.time()
         self._num_strat=0
         self._stratege[0].start()
-        self._stratege[0].start()
+
 
 
     def update(self):
         if self.stop():
             self._robot.avancer(0)
         if self._stratege[self._num_strat].stop() and self._num_strat==0:
+            print("1")
             self._num_strat=1
             self._stratege[self._num_strat].start()
             self._stratege[self._num_strat].update()
         elif self._stratege[self._num_strat].stop() and self._num_strat==1:
+            print("2")
             self._i+=1
             self._num_strat=0
             if not self.stop():
                 self._stratege[self._num_strat].start()
                 self._stratege[self._num_strat].update()
         else:
-             self._stratege[self._num_strat].update()
+            self._stratege[self._num_strat].update()
 
 class Strategie_fonce:
     def __init__(self,robot,vitesse,distance,fps=25):
