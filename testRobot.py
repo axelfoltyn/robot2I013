62d1e3e3b651161ea09eef8cd8b243139b621126
Axel Foltyn <axel.foltyn@epita.fr>
2019-03-25 16:30:35 +0100

    add testRobot.py

--- testRobot.py
+++ testRobot.py
@@ -1,6 +1,6 @@
 import time
 from Projet import Strategie_fonce, Strategie_carre
-from robot2i012 import Robot2i013 as RobotV
+from robot2I013 import Robot2I013 as RobotV
 from Projet import Adapter as Robot
 from Projet import lecture
 from Projet import View
