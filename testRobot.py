4e2c946131b65ceb45dd8d456def9aeeed822b93
Axel Foltyn <axel.foltyn@epita.fr>
2019-03-25 16:27:48 +0100

    add testRobot.py

---
+++ testRobot.py
@@ -0,0 +1,26 @@
+import time
+from Projet import Strategie_fonce, Strategie_carre
+from robot2i012 import Robot2i013 as RobotV
+from Projet import Adapter as Robot
+from Projet import lecture
+from Projet import View
+from threading import Thread
+
+robotv = RobotV()
+
+view = View(robotv._arene)
+robotv._arene.add_obs(view)
+robot = Robot(robotv)
+
+thread_affichage = Thread(target=robotv._arene.boucle_actualiser, args=(25.,))
+thread_affichage.start()
+view.start()
+
+strat = Strategie_fonce(robot, 100, 1)
+#strat = Strategie_carre(robot, 100, 100)
+
+
+strat.start()
+while not strat.stop():
+    strat.update()
+    time.sleep(1/25)
