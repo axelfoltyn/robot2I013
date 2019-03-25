0a2524ccf3f2163df80b9c40db5e9cdc5d30ed03
vicvance188 <vic188@hotmail.fr>
2019-03-25 19:22:51 +0100

    debug robot2I013

--- testRobot.py
+++ testRobot.py
@@ -2,19 +2,46 @@ import time
 from Projet import Strategie_fonce, Strategie_carre
 from robot2I013 import Robot2I013 as RobotV
 from Projet import Adapter as Robot
-from Projet import lecture
-from Projet import View
-from threading import Thread
 
 robotv = RobotV()
 robot = Robot(robotv)
 
+class strat_test:
+    def __init__(self, robot, angle, vitesse,fps=25):
+        self._robot=robot
+        self._angle=angle
+        self._vitesse=vitesse
 
-strat = Strategie_fonce(robot, 100, 1)
-#strat = Strategie_carre(robot, 100, 100)
+
+    def start(self):
+        print("reset2.0 :", self._robot.get_motor_position()[1])
+        self._robot.offset_motor_encoder(self._robot.MOTOR_RIGHT, self._robot.get_motor_position()[1])
+        print("reset2.0 :", self._robot.get_motor_position()[1])
+
+
+    def update(self):
+        if(self.stop()):
+            self._robot.tourner_droite(0)
+        else:
+            self._robot.tourner_droite(self._vitesse)
+
+
+    def stop(self):
+        print(self._robot.get_motor_position()[1])
+        circonference_cm = self._robot.WHEEL_CIRCUMFERENCE
+        distance = 2 * abs(self._robot.get_motor_position()[1]) * circonference_cm / 360
+        deta=distance * 360.0 / self._robot.WHEEL_BASE_CIRCUMFERENCE
+        print(deta)
+        return self._angle<=deta
+
+
+#strat = Strategie_fonce(robot, 100, 30)
+#strat = Strategie_carre(robot, 10, 10)
+strat = strat_test(robot, 90, 10)
 
 
 strat.start()
 while not strat.stop():
     strat.update()
-    time.sleep(1/25)
+    time.sleep(1/50)
+robot.stop()
