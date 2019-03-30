e166d50ffbb00899cc451bd70916761e617c79ac
vicvance188 <vic188@hotmail.fr>
2019-03-31 00:00:44 +0100

    update

--- projet/__init__.py
+++ projet/__init__.py
@@ -1,4 +1,4 @@
+from .adapter import Adapter
+from .strategie import StrategieAvance, StrategieTournerDroite, StrategieTournerGauche, StrategieCarre, StrategieFonce
 from .model import RobotVirtuel, Arene, Obstacle, ObstacleCarre, ObstacleRond, lecture, ecriture2
 from .view import View
-from .strategie import StrategieAvance, StrategieTournerDroite, StrategieTournerGauche, StrategieCarre, StrategieFonce
-from .adapter import Adapter
