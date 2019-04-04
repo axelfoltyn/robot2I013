bf8abf8f2146828c075fa614cab9ec6a9ecd4df6
SophiaNdr <sophia.nador@gmail.com>
2019-04-04 12:03:55 +0200

    aaa

--- projet/model/__init__.py
+++ projet/model/__init__.py
@@ -1,5 +1,5 @@
 from .robot import RobotVirtuel
 from .arene import Arene
-from .obstacle import Obstacle, ObstacleCarre, ObstacleRond
+from .obstacle import Obstacle, ObstacleCarre, ObstacleRond, ObstacleBalise
 from .lecture import lecture
 from .ecriture import ecriture2
