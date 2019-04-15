17e05583627429300aeb9af3ad4667d46c2ed915
dyroxe <sebast-94@hotmail.fr>
2019-04-15 17:51:41 +0200

    tmesolo

--- tmesolo.py
+++ tmesolo.py
@@ -1,11 +1,13 @@
-1.1: ligne 45
+tous est sur simu_script.py
+1.1: ligne 45 de view.py
 
-2.1: dans strategies.py la class StrategieTriangle
-    possible dtre executer dans simu_script avec strat= StrategieTriangle(robot,50,30)
+2.1: dans strategies.py la classe StrategieTriangle
+    possible detre executer dans simu_script avec strat= StrategieTriangle(robot,50,30)
 
-2.2: dans strategies la class StrategiePolygone
-    strat=StrategiePolygone
+2.2: dans strategies la classe StrategiePolygone
+    strat=StrategiePolygone(robot,10,8)
 
 2.3:strategie
-    strat=StrategieMur
+    strat=StrategieMur(robot,50,60)
+    ne pas oublier de enlever les obstacle dans le fichier firchier_test.txt
 
