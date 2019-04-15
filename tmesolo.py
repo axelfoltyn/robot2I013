0aaf7f7706d21f37fb86bf07e72a678f35a5b82e
dyroxe <sebast-94@hotmail.fr>
2019-04-15 17:50:07 +0200

    tmesolo

---
+++ tmesolo.py
@@ -0,0 +1,11 @@
+1.1: ligne 45
+
+2.1: dans strategies.py la class StrategieTriangle
+    possible dtre executer dans simu_script avec strat= StrategieTriangle(robot,50,30)
+
+2.2: dans strategies la class StrategiePolygone
+    strat=StrategiePolygone
+
+2.3:strategie
+    strat=StrategieMur
+
