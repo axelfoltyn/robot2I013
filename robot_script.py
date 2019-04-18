ca5ecc5f19bfc2ad838ca4f0a10df086763a7745
vicvance188 <vic188@hotmail.fr>
2019-04-18 10:24:23 +0200

    tests robot

--- robot_script.py
+++ robot_script.py
@@ -9,10 +9,10 @@ robot=Adapter(Robot(), calibrage)
 
 #VITESSE MAX = 20.9
 
-#strat=StrategieFonceAmeliore(robot, 2, 20)
+strat=StrategieFonceAmeliore(robot, 2, 20)
 #strat=StrategieFonce(robot, 5, 20)
 #strat=StrategieFonceAmeliore(robot, 300, 40)
-strat=StrategieArcGauche(robot,10,10,360*2)
+#strat=StrategieArcGauche(robot,10,10,360*2)
 strat.start()
 while not strat.stop():
     strat.update()
