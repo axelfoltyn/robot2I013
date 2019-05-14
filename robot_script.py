8bd48389e3b17b64f38c3b641033b62c153517b2
vicvance188 <vic188@hotmail.fr>
2019-05-14 18:27:46 +0200

    ajout inversion image

--- robot_script.py
+++ robot_script.py
@@ -5,17 +5,15 @@ from projet import Adapter
 from projet import StrategieCarre, StrategieFonce, StrategieFonceAmeliore, StrategieCarreAmeliore, StrategieArcGauche, StrategieArcDroit
 
 calibrage=0.99 # defaut=1 , augmenter legerement pour devier vers la droite
+#VITESSE MAX = 20
 
 robot=Adapter(Robot(), calibrage)
+
+# sauvegarde d'image
 #time.sleep(1)
-#VITESSE MAX = 20.9
-#var=robot.get_image()
-#print(var)
-#var2=[]
-#for i in var:
-#    for j in i:
-#        var2[i][j].append(var[-i][-j])
-#img=Image.fromarray(var2).convert('RGB')
+#arry=robot.get_image()
+#img=Image.fromarray(arry).convert('RGB')
+#img=img.rotate(180)
 #img.save('image.jpg')
 
 
