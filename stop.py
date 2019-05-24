f88b2787aa4bbc3a24cca23bbbca9c7d651047ad
vicvance188 <vic188@hotmail.fr>
2019-05-24 17:53:46 +0200

    merge

--- stop.py
+++ stop.py
@@ -1,6 +1,4 @@
 from robot2I013 import Robot2I013
-robot=Robot2I013()
-robot.set_motor_dps(3,0)
-robot.offset_motor_encoder(robot.MOTOR_LEFT, robot.get_motor_position()[0])
-robot.offset_motor_encoder(robot.MOTOR_RIGHT, robot.get_motor_position()[1])
-robot.servo_rotate(30)
+r=Robot2I013()
+r.set_motor_dps(3,0)
+r.servo_rotate(30)
